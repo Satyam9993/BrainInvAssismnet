7670cacf3965241f64382c09217da970309acffb
satyamkatiyar <satyamkatiyar9993@gmail.com>
2024-03-23 13:18:38 +0530

    remove files

--- firebaseConfig .js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAolrMHZt0aXLjD_0kjT6jOtHAnsHtEPDs",
-  authDomain: "braininv-680f7.firebaseapp.com",
-  projectId: "braininv-680f7",
-  storageBucket: "braininv-680f7.appspot.com",
-  messagingSenderId: "53230956342",
-  appId: "1:53230956342:web:aa0b4eafa691723c85cd97"
-};
